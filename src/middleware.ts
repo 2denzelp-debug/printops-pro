82bd165b22bf300ca4a93c2364560285c8f25385
2denzelp-debug <2denzel.p@gmail.com>
2026-04-21 20:15:31 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
-export function middleware() {
+export function middleware(request: NextRequest) {
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: []
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 }
